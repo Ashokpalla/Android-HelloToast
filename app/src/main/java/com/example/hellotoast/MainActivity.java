a9ff94ad0441b4b64cc80ac6ae5d322ce2c4368c
Ashokpalla <ashokpalla488@gmail.com>
2020-09-10 16:01:22 +0530

    first commit

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -7,24 +7,47 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.concurrent.CountedCompleter;
+
 public class MainActivity extends AppCompatActivity {
-    int count = 0;
-    TextView tv;
+
+    private int isiCount=0;
+    private int data = 10;
+    private int a=10;
+    private TextView tampilCount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv=findViewById(R.id.dataTextview);
-    }
+        tampilCount = (TextView) findViewById(R.id.textView);
 
-    public void displayToast(View view) {
-        Toast.makeText(this,"Welcome to Android Workshop",Toast.LENGTH_LONG).show();
 
     }
 
-    public void increaseCount(View view) {
-        count++;
-        tv.setText(""+count);
+    public void reset(View view) {
+        isiCount = 0;
+        data = 10;
+        Toast reset = Toast.makeText(this,"Data is Reseted",Toast.LENGTH_LONG);
+        reset.show();
+        if (tampilCount!=null){
+            tampilCount.setText(Integer.toString(isiCount));
+        }
+    }
+
+    public void count(View view) {
+        isiCount++;
+        if (isiCount==data){
+            Toast count = Toast.makeText(this,"you're Data is "+data+"",Toast.LENGTH_LONG);
+            count.show();
+        }if (tampilCount!=null){
+            tampilCount.setText(Integer.toString(isiCount));
+        }
+    }
+
+    public void view(View view) {
+        Toast isi = Toast.makeText(this,"you're Data is "+isiCount+"",Toast.LENGTH_LONG);
+        isi.show();
     }
 }
